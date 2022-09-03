ee1afff42c5b921d519c4f91ccd99261dd6cbe2d
JieLuo <jieluo129@gmail.com>
2022-09-03 13:18:29 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Current time is " + DateTime.Now);
+{
+  int a = 0;
+}
+//Console.WriteLine(a);
