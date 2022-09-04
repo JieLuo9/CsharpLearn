4103e2f7a970180fac6ef45157d401896560dc34
JieLuo <jieluo129@gmail.com>
2022-09-04 17:31:25 +0800

    9/4/2022 learn over.

--- Program.cs
+++ Program.cs
@@ -4,18 +4,41 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Collections;
 
-string[] names = { "Bob", "Otto", "Kavin" };
-foreach (string name in names)
+// string[] names = { "Bob", "Otto", "Kavin" };
+// foreach (string name in names)
+// {
+//   System.Console.WriteLine(name);
+// }
+
+
+// System.Console.WriteLine("this is IEnumerator.");
+// IEnumerator e = names.GetEnumerator();
+// while (e.MoveNext())
+// {
+//   string name = (string)e.Current;
+//   System.Console.WriteLine(name);
+// }
+
+System.Console.Write("Please input a number: ");
+string? input = Console.ReadLine();
+
+try
 {
-  System.Console.WriteLine(name);
+  int number = int.Parse(input);
+  System.Console.WriteLine($"your input number is {number}");
+}
+catch (System.FormatException) when (input.Contains("$"))
+{
+  System.Console.WriteLine("input contains invaild char.");
 }
 
-
-System.Console.WriteLine("this is IEnumerator.");
-IEnumerator e = names.GetEnumerator();
-while (e.MoveNext())
+catch (System.OverflowException)
 {
-  string name = (string)e.Current;
-  System.Console.WriteLine(name);
+  System.Console.WriteLine("your input too big or too small.");
 }
 
+catch (System.Exception e)
+{
+  System.Console.WriteLine($"{e.GetType()}: {e.Message}");
+  throw;
+}
