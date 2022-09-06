6b2766fda66fd521adf5825a7831becffccd9e9f
JieLuo <jieluo129@gmail.com>
2022-09-06 23:10:35 +0800

    09/06/2022 learn over.

---
+++ MyClass.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace myNameSpace
+{
+
+  #region myclass
+  class myClass
+  {
+    public static void myFunction()
+    {
+      System.Console.WriteLine("myNameSpace.myClass.myFunction.");
+    }
+  }
+  #endregion
+
+
+
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.Design.Serialization;
 using System.Linq.Expressions;
 using System.Text;
 using System.Collections;
+using myNameSpace;
 
 // string[] names = { "Bob", "Otto", "Kavin" };
 // foreach (string name in names)
@@ -19,26 +20,40 @@ using System.Collections;
 //   System.Console.WriteLine(name);
 // }
 
-System.Console.Write("Please input a number: ");
-string? input = Console.ReadLine();
+// System.Console.Write("Please input a number: ");
+// string? input = Console.ReadLine();
+// int? number;
 
-try
-{
-  int number = int.Parse(input);
-  System.Console.WriteLine($"your input number is {number}");
-}
-catch (System.FormatException) when (input.Contains("$"))
-{
-  System.Console.WriteLine("input contains invaild char.");
-}
+// try
+// {
+//   number = int.Parse(input);
+//   System.Console.WriteLine($"your input number is {number}");
+// }
+// catch (System.FormatException) when (input.Contains("$"))
+// {
+//   System.Console.WriteLine("input contains invaild char.");
+// }
 
-catch (System.OverflowException)
-{
-  System.Console.WriteLine("your input too big or too small.");
-}
+// catch (System.OverflowException)
+// {
+//   System.Console.WriteLine("your input too big or too small.");
+// }
 
-catch (System.Exception e)
-{
-  System.Console.WriteLine($"{e.GetType()}: {e.Message}");
-  throw;
-}
+// catch (System.Exception e)
+// {
+//   System.Console.WriteLine($"{e.GetType()}: {e.Message}");
+//   throw;
+// }
+
+myClass test = new myClass();
+
+myClass.myFunction();
+
+int a = 10;
+int b = a;
+b = 12;
+System.Console.WriteLine($"a={a}");
+string c = "ccc";
+string d = c;
+d = "ddd";
+System.Console.WriteLine($"c={c}");
